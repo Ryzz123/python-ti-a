8b48653227816f7de67927527396e56534db93af
RyzzTech <102559807+Ryzz123@users.noreply.github.com>
2022-12-06 11:13:38 +0700

    update

---
+++ pertemuan2.py
@@ -0,0 +1,38 @@
+# ==========|---CONTOH VARIABEL---|==============
+ayam = "Berjenis Unggas"
+angka = 123456789
+
+# mengakses variabel(menampilkan)
+print(ayam)
+print(angka)
+print(ayam, +angka)
+
+# Output :
+# Berjenis Unggas
+# 123456789
+# Berjenis Unggas 123456789
+
+# ==========|---CONTOH PERATURAN PENULISAN VARIABEL---|==============
+mobilku = "Pajero"
+print(mobilku)
+_mobil = "Expander"
+print(_mobil)
+mobil_nya = "Fortuner"
+print(mobil_nya)
+
+# Output :
+# Pajero
+# Expander
+# Fortuner
+
+# ==========|---CONTOH PENULISAN KONSTANTA DI PYTHON ---|==============
+AYAM = "Ayam berjenis hewan unggas"
+BEBEK = "Bebek berjenis unggas"
+print(AYAM)
+print(BEBEK)
+print(AYAM,BEBEK)
+
+# Output :
+# Ayam berjenis hewan unggas
+# Bebek berjenis unggas
+# Ayam berjenis hewan unggas Bebek berjenis unggas
