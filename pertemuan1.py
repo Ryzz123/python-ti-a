22a562ca24a7bb799158fca5ce00a87a261d7bd4
RyzzTech <102559807+Ryzz123@users.noreply.github.com>
2022-12-06 10:18:06 +0700

    first commit

---
+++ pertemuan1.py
@@ -0,0 +1,43 @@
+# Print Berfungsi Untuk Mencetak Di console
+print("Saya Belajar Algoritma")
+
+# Berfungsi Membuat Variabel Di Python
+mawar = "Bunga Mawar Berduri"
+print(mawar)
+
+# Berfungsi Pengulangan Indentasi
+for Belajar in range (0,5):
+    print(Belajar)
+    if(Belajar >= 2):
+        print("Belajar Indentasi")
+
+
+# Materi Kedua
+#  Nama     : Febri Ananda Lubis
+#  Mata Kuliah : Struktur Data
+#  NPM : 2208100024
+#  Materi : Struktur Data
+print("Selamat Datang Di Python") # Untuk Menampilkan Selamat Datang di Layar Program
+for a in range (0,2):
+    print("Buah Apel") # Menghasilkan Output Buah Apel
+
+# Kasus
+# Buatlah program python pada Matakuliah Logika/algoritma
+# Tampilkan Biodat Anda
+nama = "Febri Ananda Lubis"
+alamat = "Emplasmen Turi Kebun BERANGIR"
+umur = "18"
+status = "Mahasiswa"
+universitas = "ULB"
+
+print(nama)
+print(alamat)
+print(umur)
+print(status)
+print(universitas)
+
+# Tampilkan Jenis Jenis Sepeda Motor
+print("Honda")
+print("Yamaha")
+print("Suzuki")
+print("Kawasaki")
